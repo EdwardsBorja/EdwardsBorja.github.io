7ad09d4929ddfb7d2b1f3b34bbe3bcadb0c19b04
Edwards Borja <100376168+EdwardsBorja@users.noreply.github.com>
2024-03-12 18:09:00 -0600

    Add files via upload

---
+++ scripts/ejercicio.js
@@ -0,0 +1,15 @@
+function CambiarColor(){
+    variable_album=document.getElementById("album")
+    variable_album.style.backgroundColor= "chartreuse";
+
+
+
+}
+
+function cambiarTamano(){
+    variable_imagen=document.getElementsByClassName("imagen Disco_origins");
+    console.log(clase_imagen)
+    variable.style.width = '156px';
+    
+
+}
