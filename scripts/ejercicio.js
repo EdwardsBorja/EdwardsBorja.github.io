77c7522a5f1e4e073db42ef36f77a8267aa3c727
Edwards Borja <100376168+EdwardsBorja@users.noreply.github.com>
2024-03-19 19:24:39 -0600

    Add files via upload

---
+++ scripts/ejercicio.js
@@ -0,0 +1,30 @@
+function CambiarColor(){
+    variable_album=document.getElementById("album")
+    variable_album.style.backgroundColor= "blue";
+
+
+
+}
+
+function cambiarTamanoImagen() {
+    var imagen = document.querySelector('.imagen');
+    // Cambiar el tamaño de la imagen
+    imagen.style.width = "150px"; // Cambiar el ancho a 150px
+    imagen.style.height ="150px";
+}
+
+
+
+/*function cambiarTamano(){
+    var elemento = document.getElementsByClassName("imagen");
+    elemento.style.width = "10px";
+    elemento.style.height = "10px";
+    
+
+}
+//function encoger(){
+    //var foto=document.getElementsByClassName("imagen");
+    //foto.style.width="5px";
+    //foto.style.height = "5px";
+    //document.getElementById("variable").innerHTML=document.getElementById("foto").style.width;
+//}*/
